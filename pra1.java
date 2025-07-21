43c9753fef253122a83a5b128fae338fdfee984a
Tanishka Sain <133621980+Tanishka20102004@users.noreply.github.com>
2025-07-21 11:53:52 +0530

    pra1.java

---
+++ pra1.java
@@ -0,0 +1,21 @@
+public class pra1 {
+    public static void main(String[] args){
+        int a=1;
+        int b=2;
+
+        int mul=a*b;
+        System.out.println(mul);
+
+        int diff= b-a;
+        System.out.println(diff);
+
+        int add=a+b;
+        System.out.println(add);
+
+        int div=b/a;
+        System.out.println(div);
+
+        int ans=a*b/b-a;
+        System.out.println(ans);
+    }
+}
